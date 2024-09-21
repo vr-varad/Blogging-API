8592eace671732141d0b2a786dfd9cbb9d0e73be
vr-varad <varadgupta21#gmail.com>
2024-09-22 02:56:58 +0530

    feat: Rate Limiting

--- src/express-app.ts
+++ src/express-app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 import { UserRouter, BlogRouter } from './routes'
+import Limiter from './utils/rateLimiting'
 
 const expressApp = (app: Application) => {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
     app.use(cors())
+    app.use(Limiter)
     app.use('/api/v1/user', UserRouter)
     app.use('/api/v1/blogs', BlogRouter)
 }
---
+++ src/utils/rateLimiting.ts
@@ -0,0 +1,9 @@
+import { rateLimit } from 'express-rate-limit'
+const Limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false
+})
+
+export default Limiter
